7f4df581bc886a01693d4465c5cc09d050f4325d
ivpankaj <imvpankaj@gmail.com>
2026-07-24 09:59:49 +0530

    new server

--- check.js
+++ check.js
@@ -1,16 +1,17 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(async () => {
     const db = mongoose.connection.db;
-    const users = await db.collection('users').find({}).toArray();
+    const users = await db.collection("users").find({}).toArray();
     console.log(JSON.stringify(users, null, 2));
     process.exit(0);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
     process.exit(1);
   });
